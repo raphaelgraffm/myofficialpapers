7b2c9e72e9628c9685ac606fedf588d3255ca0e3
root <root@vps322568.ovh.net>
2016-10-02 21:30:55 +0200

    Additions sur le serveur

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Atelier\CoreBundle\AtelierCoreBundle(),
             new MOP\CoreBundle\MOPCoreBundle(),
         ];
 
